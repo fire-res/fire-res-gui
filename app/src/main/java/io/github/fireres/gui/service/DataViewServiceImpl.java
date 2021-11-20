a8ec91034ad9a822752e1267fb602b689f5186a8
rfatnev <r.fatnev@gmail.com>
2021-11-20 13:08:38 +0300

    [feat/FR-37] Fixed excel styles

--- app/src/main/java/io/github/fireres/gui/service/DataViewServiceImpl.java
+++ app/src/main/java/io/github/fireres/gui/service/DataViewServiceImpl.java
@@ -6,9 +6,10 @@ import io.github.fireres.excel.core.builder.ExcelReportsBuilder;
 import io.github.fireres.gui.component.DataViewer;
 import io.github.fireres.gui.framework.service.DataViewService;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
+import java.util.List;
 
 import static java.util.Collections.singletonList;
 
@@ -16,15 +17,20 @@ import static java.util.Collections.singletonList;
 @RequiredArgsConstructor
 public class DataViewServiceImpl implements DataViewService {
 
-    private final Map<ReportType, ExcelReportsBuilder> reportBuilders;
+    private final List<ExcelReportsBuilder> reportsBuilders;
 
     @Override
     public DataViewer getDataViewer(Report<?> report) {
-        return new DataViewer(
-                reportBuilders.get(report.getType())
-                        .build(singletonList(report))
-                        .get(0)
-        );
+        val reportBuilder = lookupReportBuilder(report.getType());
+
+        return new DataViewer(reportBuilder.build(singletonList(report)).get(0));
+    }
+
+    private ExcelReportsBuilder lookupReportBuilder(ReportType reportType) {
+        return reportsBuilders.stream()
+                .filter(builder -> builder.supportedReportType().equals(reportType))
+                .findFirst()
+                .orElseThrow();
     }
 
 }
