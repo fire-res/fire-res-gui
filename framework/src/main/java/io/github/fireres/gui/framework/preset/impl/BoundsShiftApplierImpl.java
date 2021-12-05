12e1f900bef490dc47f7312b4029ce13e0b40fdf
Dinir Yakupov <dinir95@gmail.com>
2021-12-05 20:47:29 +0500

    [FR-34] Убирает логгер

--- framework/src/main/java/io/github/fireres/gui/framework/preset/impl/BoundsShiftApplierImpl.java
+++ framework/src/main/java/io/github/fireres/gui/framework/preset/impl/BoundsShiftApplierImpl.java
@@ -5,12 +5,10 @@ import io.github.fireres.gui.framework.controller.common.BoundShift;
 import io.github.fireres.gui.framework.controller.common.BoundsShiftParams;
 import io.github.fireres.gui.framework.preset.BoundsShiftApplier;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class BoundsShiftApplierImpl implements BoundsShiftApplier {
 
     @Override
