f9cc769593b91a519dbc1575568d9f7ef4713e9d
therealmone <36332304+therealmonE@users.noreply.github.com>
2021-12-05 22:56:36 +0300

    [fix] Fixed file encoding

--- app/src/main/java/io/github/fireres/gui/config/ObjectMapperConfig.java
+++ app/src/main/java/io/github/fireres/gui/config/ObjectMapperConfig.java
@@ -21,6 +21,10 @@ public class ObjectMapperConfig {
             UnheatedSurfaceProperties.class
     };
 
+    static {
+        System.setProperty("file.encoding", "UTF-8");
+    }
+
     @Bean
     public ObjectMapper getObjectMapper() {
         val mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
